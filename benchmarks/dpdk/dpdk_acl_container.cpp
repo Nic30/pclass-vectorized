30dcf658afb65f83fbfccca42c668486e21c5a02
nic30 <nic30@seznam.cz>
2019-04-24 16:57:28 +0100

    dpdk: fix endianity in the field defs

--- benchmarks/dpdk/dpdk_acl_container.cpp
+++ benchmarks/dpdk/dpdk_acl_container.cpp
@@ -1,29 +1,34 @@
+#include <rte_acl.h>
+
+#include <algorithm>
+#include <string>
+#include <byteswap.h>
+
+#include <pcv/rule_parser/rule.h>
+#include <pcv/rule_parser/classbench_rule_parser.h>
+
+#include "log.h"
 #include "dpdk_acl_container.h"
 #include "dpdk_acl_rule_dump.h"
 #include "dpdk_acl_field_defs.h"
-#include "log.h"
-#include <rte_acl.h>
-
-#include <string>
-#include <pcv/rule_parser/rule.h>
-#include <pcv/rule_parser/classbench_rule_parser.h>
 
 using namespace pcv;
 
 namespace dpdk_acl_configs {
 
 template<typename RULE_T>
-void get_config(struct rte_acl_config * ctx);
+void get_config(struct rte_acl_config * ctx, size_t num_categories);
 
 template<>
-void get_config<pcv::Rule_Ipv4_ACL>(struct rte_acl_config * cfg) {
+void get_config<pcv::Rule_Ipv4_ACL>(struct rte_acl_config * cfg,
+		size_t num_categories) {
 	memcpy(&cfg->defs, ipv4_defs, sizeof(ipv4_defs));
 	cfg->num_fields = RTE_DIM(ipv4_defs);
 
 	for (int i = 0; i < 5; i++) {
 		cfg->defs[i].offset = ipv4_tuple_layout[i];
 	}
-	cfg->num_categories = 5;
+	cfg->num_categories = num_categories;
 	cfg->num_fields = 5;
 }
 
@@ -35,71 +40,59 @@ DpdkAclContainer::DpdkAclContainer(const std::vector<iParsedRule*> & rules) {
 
 template<typename RULE_T>
 void add_rules(const std::vector<iParsedRule*> & rules,
-		std::vector<RULE_T> & rules_route, std::vector<RULE_T> & rules_acl) {
+		std::vector<RULE_T> & rules_acl) {
 	RuleReader rr;
+	int i = 0;
 	for (auto r : rules) {
 		RULE_T * next;
 		auto ipv4_acl_r = dynamic_cast<Rule_Ipv4_ACL*>(r);
-		std::cerr << *ipv4_acl_r << std::endl;
-		if (ipv4_acl_r) {
-			rules_acl.push_back( { });
-			next = &rules_acl.back();
-			next->data.userdata = 0xf0000000 + rules_acl.size();
-			next->field[PROTO_FIELD_IPV4].value.u16 = ipv4_acl_r->proto.low;
-			next->field[PROTO_FIELD_IPV4].mask_range.u16 =
-					ipv4_acl_r->proto.get_mask_littleendian();
-			next->field[SRC_FIELD_IPV4].value.u32 = ipv4_acl_r->sip.low;
-			next->field[SRC_FIELD_IPV4].mask_range.u32 =
-					ipv4_acl_r->sip.get_mask_littleendian();
-			next->field[DST_FIELD_IPV4].value.u32 = ipv4_acl_r->dip.low;
-			next->field[DST_FIELD_IPV4].mask_range.u32 =
-					ipv4_acl_r->dip.get_mask_littleendian();
-			next->field[SRCP_FIELD_IPV4].value.u16 = ipv4_acl_r->sport.low;
-			next->field[SRCP_FIELD_IPV4].mask_range.u16 =
-					ipv4_acl_r->sport.get_mask_littleendian();
-			next->field[DSTP_FIELD_IPV4].value.u16 = ipv4_acl_r->dport.low;
-			next->field[DSTP_FIELD_IPV4].mask_range.u16 =
-					ipv4_acl_r->dport.get_mask_littleendian();
+		//std::cerr << *ipv4_acl_r << std::endl;
+		rules_acl.push_back( { });
+		next = &rules_acl.back();
+		next->data.userdata = i;
+		next->field[PROTO_FIELD_IPV4].value.u16 = ipv4_acl_r->proto.low;
+		next->field[PROTO_FIELD_IPV4].mask_range.u16 =
+				ipv4_acl_r->proto.get_mask_littleendian();
+		next->field[SRC_FIELD_IPV4].value.u32 = __bswap_32(ipv4_acl_r->sip.low);
+		next->field[SRC_FIELD_IPV4].mask_range.u32 = __bswap_32(
+				ipv4_acl_r->sip.get_mask_littleendian());
+		next->field[DST_FIELD_IPV4].value.u32 = __bswap_32(ipv4_acl_r->dip.low);
+		next->field[DST_FIELD_IPV4].mask_range.u32 = __bswap_32(
+				ipv4_acl_r->dip.get_mask_littleendian());
+		next->field[SRCP_FIELD_IPV4].value.u16 = ipv4_acl_r->sport.low;
+		next->field[SRCP_FIELD_IPV4].mask_range.u16 =
+				ipv4_acl_r->sport.get_mask_littleendian();
+		next->field[DSTP_FIELD_IPV4].value.u16 = ipv4_acl_r->dport.low;
+		next->field[DSTP_FIELD_IPV4].mask_range.u16 =
+				ipv4_acl_r->dport.get_mask_littleendian();
 
-		} else {
-			rules_route.push_back( { });
-			throw std::runtime_error(
-					std::string(__FUNCTION__) + "not implemented");
-			next = &rules_route.back();
-			/* Check the forwarding port number */
-			next->data.userdata += FWD_PORT_SHIFT;
-		}
-
-		next->data.priority = RTE_ACL_MAX_PRIORITY
-				- (rules_route.size() + rules_acl.size());
+		next->data.priority = RTE_ACL_MAX_PRIORITY - rules_acl.size();
 		next->data.category_mask = -1;
+		i++;
 	}
 }
 
 void DpdkAclContainer::acl_init(const std::vector<iParsedRule*> & rules) {
-	std::vector<acl4_rule> rules_acl, rules_route;
+	std::vector<acl4_rule> rules_acl;
 
 	/* Load  rules from the input file */
-	add_rules<acl4_rule>(rules, rules_acl, rules_route);
-	memset(&acl_config, 0, sizeof(acl_config));
-
-	acl_config.acx_ipv4 = setup_acl(rules_route, rules_acl, 0);
+	add_rules<acl4_rule>(rules, rules_acl);
+	acl_ctx = setup_acl(rules_acl, 0);
 }
 
 struct rte_acl_ctx*
-DpdkAclContainer::setup_acl(const std::vector<acl4_rule> rules_acl,
-		const std::vector<acl4_rule> rules_route, int socketid) {
-	char name[PATH_MAX];
+DpdkAclContainer::setup_acl(const std::vector<acl4_rule> & rules_acl,
+		int socketid) {
 	struct rte_acl_param acl_param;
 	struct rte_acl_config acl_build_param;
-	dpdk_acl_configs::get_config<pcv::Rule_Ipv4_ACL>(&acl_build_param);
+	dpdk_acl_configs::get_config<pcv::Rule_Ipv4_ACL>(&acl_build_param,
+			NUM_CATEGORIES);
 	struct rte_acl_ctx *context;
 	int dim = RTE_DIM(ipv4_defs);
 
 	/* Create ACL contexts */
-	snprintf(name, sizeof(name), "%s%d", L3FWD_ACL_IPV4_NAME, socketid);
 
-	acl_param.name = name;
+	acl_param.name = ACL_NAME;
 	acl_param.socket_id = socketid;
 	acl_param.rule_size = RTE_ACL_RULE_SZ(dim);
 	acl_param.max_rule_num = std::numeric_limits<uint16_t>::max() - 1;
@@ -111,12 +104,6 @@ DpdkAclContainer::setup_acl(const std::vector<acl4_rule> rules_acl,
 		rte_exit(EXIT_FAILURE,
 				"Failed to setup classify method for  ACL context\n");
 
-	if (rules_route.size()) {
-		if (rte_acl_add_rules(context,
-				(const struct rte_acl_rule*) &rules_route[0],
-				rules_route.size()) < 0)
-			rte_exit(EXIT_FAILURE, "add rules failed\n");
-	}
 	if (rules_acl.size()) {
 		if (rte_acl_add_rules(context,
 				(const struct rte_acl_rule*) &rules_acl[0], rules_acl.size())
@@ -130,27 +117,56 @@ DpdkAclContainer::setup_acl(const std::vector<acl4_rule> rules_acl,
 	return context;
 }
 
-uint16_t DpdkAclContainer::search(std::array<uint16_t, 7> & val) {
-	uint32_t res_cls;
-	const uint8_t * data[1] = { (uint8_t *) &val[0] };
-	auto res = rte_acl_classify(acl_config.acx_ipv4, data, &res_cls, 1, 1);
+uint16_t DpdkAclContainer::search(std::array<uint16_t, 7> & val_big_endian) {
+	std::array<uint32_t, NUM_CATEGORIES> res_cls;
+	const uint8_t * data[1] = { (uint8_t *) &val_big_endian[0] };
+	auto res = rte_acl_classify(acl_ctx, data, &res_cls[0], 1, 1);
 	if (res < 0) {
 		throw std::runtime_error(
 				std::string("Error in rte_acl_classify ")
 						+ std::to_string(res));
 	}
-	return res_cls;
+	std::cout << "search:";
+	for (auto i : res_cls) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	return (uint16_t) *std::max_element(res_cls.begin(), res_cls.end());
 }
 
 DpdkAclContainer::~DpdkAclContainer() {
-	rte_acl_free(acl_config.acx_ipv4);
+	rte_acl_free(acl_ctx);
 }
 
-//void DpdkAclContainer::dump() {
+/*
+ * Original structure from dpdk is not in public headers and this is only beggining of it
+ * */
+struct __rte_acl_ctx__only_part {
+	char                name[RTE_ACL_NAMESIZE];
+	/** Name of the ACL context. */
+	int32_t             socket_id;
+	/** Socket ID to allocate memory from. */
+	enum rte_acl_classify_alg alg;
+	void               *rules;
+	uint32_t            max_rules;
+	uint32_t            rule_sz;
+	uint32_t            num_rules;
+	uint32_t            num_categories;
+	uint32_t            num_tries;
+	uint32_t            match_index;
+	uint64_t            no_match;
+	uint64_t            idle;
+	uint64_t           *trans_table;
+	uint32_t           *data_indexes;
+};
+
+int DpdkAclContainer::get_number_of_tries() {
+	return ((__rte_acl_ctx__only_part *)acl_ctx)->num_tries;
+}
+
+void DpdkAclContainer::dump() {
 //	acl_log("IPv4 Route entries %zu:\n", rules_route.size());
 //	dump_ipv4_rules((acl4_rule *) &rules_route[0], rules_route.size(), 1);
 //
-//	acl_log("IPv4 ACL entries %zu:\n", rules_acl.size());
-//	dump_ipv4_rules((acl4_rule *) &rules_acl[0], rules_acl.size(), 1);
-// rte_acl_dump(context);
-//}
+	rte_acl_dump(acl_ctx);
+}
--- benchmarks/dpdk/dpdk_acl_container.h
+++ benchmarks/dpdk/dpdk_acl_container.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <array>
 #include <vector>
+
 #include "dpdk_acl_field_defs.h"
 #include <pcv/rule_parser/rule.h>
 
@@ -11,10 +12,7 @@
  * */
 class DpdkAclContainer {
 public:
-	struct acl_config_t {
-		struct rte_acl_ctx *acx_ipv4;
-	};
-	acl_config_t acl_config;
+	struct rte_acl_ctx *acl_ctx;
 	/*
 	 * @param rules the initial ruleset for this classifier
 	 * */
@@ -22,12 +20,14 @@ public:
 	~DpdkAclContainer();
 
 	uint16_t search(std::array<uint16_t, 7> & val);
+	void dump();
+	int get_number_of_tries();
 
 private:
-	static constexpr const char *L3FWD_ACL_IPV4_NAME = "l3fwd-acl-ipv4";
+	static constexpr const char *ACL_NAME = "acl";
+	static constexpr const size_t NUM_CATEGORIES = 1;
 
 	void acl_init(const std::vector<pcv::iParsedRule*> & rules);
-	struct rte_acl_ctx* setup_acl(const std::vector<acl4_rule> rules_acl,
-			const std::vector<acl4_rule> rules_route, int socketid);
-	//void dump();
+	struct rte_acl_ctx* setup_acl(const std::vector<acl4_rule> & rules_acl,
+			int socketid);
 };

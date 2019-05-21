8e12c603028c2f72891b134ffbb2d11b20fa8165
nic30 <nic30@seznam.cz>
2019-05-21 22:37:37 +0100

    add test for Range1d endianity utils

---
+++ tests/range_test.cpp
@@ -0,0 +1,59 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE pcv_test
+
+#include "test_common.h"
+
+#include <limits>
+#include <pcv/common/range.h>
+
+using namespace std;
+using namespace pcv;
+
+BOOST_AUTO_TEST_SUITE( pcv_testsuite )
+BOOST_AUTO_TEST_CASE( masks ) {
+	{
+		Range1d<uint32_t> a;
+		a.low = 0xff000000;
+		a.high = 0xffffffff;
+		auto m = a._get_mask_le();
+		BOOST_CHECK_EQUAL(m.first, 0xff000000);
+		BOOST_CHECK_EQUAL(m.second, 8);
+
+		auto b = a.to_be();
+		BOOST_CHECK_EQUAL(b.low, 0x000000ff);
+	}
+	{
+		Range1d<uint32_t> a;
+		a.low = 0xffff0000;
+		a.high = 0xffffffff;
+		auto m = a._get_mask_le();
+		BOOST_CHECK_EQUAL(m.first, 0xffff0000);
+		BOOST_CHECK_EQUAL(m.second, 16);
+
+	}
+	{
+		Range1d<uint32_t> a;
+		a.low = 0x0000ffff;
+		a.high = 0xffffffff;
+		auto m = a._get_mask_be();
+		BOOST_CHECK_EQUAL(m.first, 0x0000ffff);
+		BOOST_CHECK_EQUAL(m.second, 16);
+	}
+	{
+		Range1d<uint16_t> a;
+		a.low = 0xfff0;
+		a.high = 0xffff;
+		auto m = a._get_mask_be();
+		BOOST_CHECK_EQUAL(m.first, 0xf0);
+		BOOST_CHECK_EQUAL(m.second, 4);
+		m = a._get_mask_le();
+		BOOST_CHECK_EQUAL(m.first, 0xfff0);
+		BOOST_CHECK_EQUAL(m.second, 12);
+	}
+
+}
+
+//____________________________________________________________________________//
+
+BOOST_AUTO_TEST_SUITE_END()

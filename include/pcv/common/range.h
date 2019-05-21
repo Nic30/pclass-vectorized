dc8d8b4b12ff4eed8b6ae60c2b681de4a5963a5c
nic30 <nic30@seznam.cz>
2019-05-21 22:34:38 +0100

    Range1d: constructor with the mask, endianity utils

--- include/pcv/common/range.h
+++ include/pcv/common/range.h
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <limits>
 #include <byteswap.h>
+#include <tuple>
 #include <linux/byteorder/little_endian.h>
 
 namespace pcv {
@@ -24,6 +25,15 @@ inline uint64_t to_be(uint64_t val) {
 
 template<typename T>
 class Range1d {
+	// https://stackoverflow.com/questions/3313909/finding-the-length-of-the-common-prefix-in-two-bytes
+	static int bytePrefix[256];
+	static uint8_t mask_val[9];
+	static std::pair<int, uint8_t> get_common_prefix_length(uint8_t a,
+			uint8_t b) {
+		auto pl = bytePrefix[a ^ b];
+		return {pl, mask_val[pl]};
+	}
+
 public:
 	T low;
 	T high;
@@ -33,6 +43,9 @@ public:
 	Range1d(T low, T high) :
 			low(low), high(high) {
 		assert(low <= high);
+	}
+	static Range1d from_mask(T low, T mask) {
+		return Range1d(low, low | ~mask);
 	}
 	/*
 	 * @note overlap not checked
@@ -62,28 +75,54 @@ public:
 				or (other.low <= low and other.high >= high);
 	}
 
+	T get_mask_le() const {
+		return _get_mask_le().first;
+	}
+	T get_mask_be() const {
+		return _get_mask_be().first;
+	}
 	/*
 	 * Extract mask from the rule
 	 **/
-	T get_mask_littleendian() const {
-		// [TODO] highly sub optimal use std::mismatch
-		T msb = 1;
-		msb <<= (sizeof(T) * 8 - 1);
-
-		T m = 0;
-		T last_mask = m;
-		for (size_t i = 0; i < sizeof(T) * 8; i++) {
-			m >>= 1;
-			m |= msb;
-			if ((low & m) != (high & m)) {
+	std::pair<T, size_t> _get_mask_le() const {
+		// [TODO] sub optimal use std::mismatch
+		T mask = 0;
+		size_t prefix_len = 0;
+		auto m = reinterpret_cast<uint8_t *>(&mask);
+		auto l = reinterpret_cast<const uint8_t *>(&low);
+		auto h = reinterpret_cast<const uint8_t *>(&high);
+		for (int b = sizeof(T) - 1; b >= 0; b--) {
+			int pl;
+			uint8_t pm;
+			std::tie(pl, pm) = get_common_prefix_length(l[b], h[b]);
+			m[b] = pm;
+			prefix_len += pl;
+			if (pl != 8) {
 				break;
 			}
-			last_mask = m;
+
 		}
-		return last_mask;
+		return {mask, prefix_len};
 	}
-	T get_mask_bigendian() const {
-		return endianity_swap::to_be(to_be().get_mask_littleendian());
+	std::pair<T, size_t> _get_mask_be() const {
+		// [TODO] sub optimal use std::mismatch
+		T mask = 0;
+		size_t prefix_len = 0;
+		auto m = reinterpret_cast<uint8_t *>(&mask);
+		auto l = reinterpret_cast<const uint8_t *>(&low);
+		auto h = reinterpret_cast<const uint8_t *>(&high);
+		for (size_t b = 0; b < sizeof(T); b++) {
+			int pl;
+			uint8_t pm;
+			std::tie(pl, pm) = get_common_prefix_length(l[b], h[b]);
+			m[b] = pm;
+			prefix_len += pl;
+			if (pl != 8) {
+				break;
+			}
+
+		}
+		return {mask, prefix_len};
 	}
 	Range1d to_be() const {
 		return Range1d(endianity_swap::to_be(low), endianity_swap::to_be(high));
@@ -98,7 +137,32 @@ public:
 		low = 0;
 		high = std::numeric_limits<T>::max();
 	}
+	size_t prefix_len_le() {
+		return _get_mask_le().second;
+	}
+	size_t prefix_len_be() {
+		return to_be().prefix_len_le();
+	}
+	size_t max_prefix_len() {
+		return sizeof(T) * 8;
+	}
 
 };
 
+template<typename T>
+int Range1d<T>::bytePrefix[256] = { 8, 7, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4,
+		4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2,
+		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+		2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0 };
+template<typename T>
+uint8_t Range1d<T>::mask_val[9] = { 0, 0b10000000, 0b11000000, 0b11100000, 0xf0, 0b11111000, 0b11111100, 0b11111110,
+		0xff };
 }

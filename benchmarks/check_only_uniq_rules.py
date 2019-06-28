38ec413c76999f9272a7bfa4c66f7fc7b553b9f5
nic30 <nic30@seznam.cz>
2019-06-28 23:41:53 +0100

    check_only_uniq_rules: filter non unique

--- benchmarks/check_only_uniq_rules.py
+++ benchmarks/check_only_uniq_rules.py
@@ -7,7 +7,16 @@ def check_only_uniq_rules(rule_lines):
             print("duplicit:" + rl)
         seen.add(rl)
 
+def uniq(rule_lines):
+    seen = set()
+    for rl in rule_lines:
+        rl = rl.strip()
+        if rl not in seen:
+            print(rl)
+        seen.add(rl)
+
 if __name__ == "__main__":
-    fn = "../tests/data/acl1_100"
+    fn = "../tests/data/acl1_500"
     with open(fn) as f:
-        check_only_uniq_rules(f.readlines())
+        uniq(f.readlines())
+        #check_only_uniq_rules(f.readlines())

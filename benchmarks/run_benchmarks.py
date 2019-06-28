79cc21eb21617fc582fe19723a2588503dcc82e6
nic30 <nic30@seznam.cz>
2019-06-28 23:50:32 +0100

    update test config

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -23,31 +23,36 @@ if __name__ == "__main__":
     conn = sqlite3.connect(DB_NAME)
     conn.execute('''CREATE TABLE IF NOT EXISTS test_result
                  (timestamp int, app_name text, ruleset_name text, flow_cnt int, packet_cnt int, real_rule_cnt int,
-                 construction_time real, number_or_tries_or_tables int, lookup_speed real)''')
+                 construction_time real, number_of_tries_or_tables int, lookup_speed real)''')
     conn.execute('''CREATE TABLE IF NOT EXISTS benchmark_execs
                  (timestamp int, revision text, machine_name text)''')
     conn.commit()
 
-    #PARALLEL = True
-    PARALLEL = False
+    PARALLEL = True
+    #PARALLEL = False
 
     CLASSBENCH_ROOT = "../classbench-ng/generated/"
-    MESON_BUILD_PATH = "build/meson.debug.linux.x86_64.1/benchmarks/"
+    MESON_BUILD_PATH = "build/meson.debug.linux.x86_64/benchmarks/"
     # (filename, requires sudo, repeat cnt)
-    REPEATS = 4
+    REPEATS = 2
     BENCHMARKS = [
         (MESON_BUILD_PATH + "1_tree_lookup", False, REPEATS),
         (MESON_BUILD_PATH + "n_tree_lookup", False, REPEATS),
-        (MESON_BUILD_PATH + "dpdk/1_tree_lookup_dpdk", True, REPEATS),
+        #(MESON_BUILD_PATH + "dpdk/1_tree_lookup_dpdk", True, REPEATS),
+        (MESON_BUILD_PATH + "ovs/ovs_pcv_lookup", False, REPEATS),
+        (MESON_BUILD_PATH + "ovs/ovs_tss_lookup", False, REPEATS),
     ]
 
     FLOW_CNTS = [
         # 1,
         16, 128,
-        1024, 4096, 8192,
+        1024,
+        4096,
+        8192,
         65536
     ]
     PACKET_CNTS = [
+        #10000,
         10000000,
         # 100000000,
     ]
@@ -56,7 +61,13 @@ if __name__ == "__main__":
     #]
     # pprint(find_all_files(CLASSBENCH_ROOT))
     # sys.exit(1)
-    rule_files = [f for f in find_all_files(CLASSBENCH_ROOT) if not f.endswith(".py")]
+    rule_files = [
+        f for f in find_all_files(CLASSBENCH_ROOT)
+        if not f.endswith(".py")
+           # this rule sets are consuming more than available memory in default mempool
+           and not f.endswith("exact0_32k")
+           and not f.endswith("ipc1_5000")
+    ]
 
     # (number of flows, number of packets)
     TEST_ARGS = list(
@@ -66,4 +77,4 @@ if __name__ == "__main__":
     run_benchmarks(DB_NAME, TEST_ARGS, PARALLEL)
 
     generate_graph_troughput_with_increasing_number_of_flows(DB_NAME)
-    generate_graphs_depending_on_ruleset_size(DB_NAME, 4096)
+    generate_graphs_depending_on_ruleset_size(DB_NAME, 4096, PACKET_CNTS[-1])

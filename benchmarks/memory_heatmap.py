f973a3e1b2b1df7b6ea256ee1d8b1ee9b5bb848f
nic30 <nic30@seznam.cz>
2019-07-09 21:04:28 +0100

    logarithmical widht of the tree in heatmaps

--- benchmarks/memory_heatmap.py
+++ benchmarks/memory_heatmap.py
@@ -2,8 +2,15 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from typing import Tuple
-from pprint import pprint
 import json
+from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+from subprocess import check_output
+import os
+from cmath import isnan, nan
+from builtins import staticmethod
+from math import log, ceil, floor
+from pprint import pprint
 
 
 class HeatMapGen():
@@ -50,74 +57,124 @@ class HeatMapGen():
         n["width"] = width = max(width, 1)
         return width
 
-    def print_nodes(self, nodes, node_id, img):
+    def print_nodes(self, nodes, node_id, width, height):
+        total_size = (height, width)
+        overlaps = np.empty(total_size)
+        overlaps[:] = 0
+        img = np.empty(total_size)
+        img[:] = np.nan
+        self._print_nodes(nodes, node_id, img, overlaps)
+        # [todo] do not know how to reliably handle NaN and 0 division in numpy
+        res = [[
+            i / o if o > 0 else nan for (i, o) in zip(cimg, coverlap)
+            ] for cimg, coverlap in zip(img, overlaps)]
+        return  np.array(res)
+
+    def _print_nodes(self, nodes, node_id, img, overlaps):
         n = nodes[node_id]
-        x = n["x"] + n["width"] // 2
-        val = (n["key_cnt"] + len(n["children"]) + len(n["next_layer"])) / 24
-        img[n["y"]][x] = val
+        x = floor(n["x"] + n["width"] / 2)
+        val = (n["key_cnt"] * 2
+               +len(n["children"])
+               +len(n["next_layer"])) / (2 * 8 + 9 + 8)
+        y = n["y"]
+
+        prev_val = img[y][x]
+        if isnan(prev_val):
+            img[y][x] = val
+        else:
+            img[y][x] += val
+        overlaps[y][x] += 1
+
         for c in n["children"]:
-            self.print_nodes(nodes, c, img)
+            self._print_nodes(nodes, c, img, overlaps)
         for c in n["next_layer"]:
-            self.print_nodes(nodes, c, img)
+            self._print_nodes(nodes, c, img, overlaps)
+
+    def to_log_width(self, nodes, node_id, log_base):
+        """
+        Resize the image to with logarithmical X axis
+        Use average values a substitution
+        """
+        n = nodes[node_id]
+        x = n["x"]
+        if x < log_base:
+            x = 0
+        else:
+            x = log(x, log_base)
+        x = floor(x)
+        n["x"] = x
+
+        x_start = x
+        x_end = x_start
+        for c in n["children"]:
+            w = self.to_log_width(nodes, c, log_base)
+            x_end = nodes[c]["x"] + w
+
+        for c in n["next_layer"]:
+            w = self.to_log_width(nodes, c, log_base)
+            x_end = nodes[c]["x"] + w
+
+        width = n["width"] = max(1, x_end - x_start)
+        return width
 
     def generate_heatmap(self):
-        x_offset = 0
-        y_max = 0
-        x_padding = 2
+        imgs = []
         for t in self.trees:
             root_id = t["root"]
             nodes = t["nodes"]
             depth = self.resolve_y(nodes, root_id, 0)
-            y_max = max(y_max, depth)
-            self.resolve_x(nodes, root_id, x_offset)
-            root = nodes[root_id]
-            x_offset += root["width"] + x_padding
+            self.resolve_x(nodes, root_id, 0)
+            # width = root["width"]
+            width = self.to_log_width(nodes, root_id, log_base=2)
+            # pprint(nodes)
 
-        total_size = (y_max + 1, x_offset + 1)
-        img = np.empty(total_size)
-        img[:] = np.nan
-        # start of the tree on X axis (left corner)
-        for t in self.trees:
-            self.print_nodes(t["nodes"],
-                             t["root"], img)
+            #print(width)
+            # start of the tree on X axis (left corner)
+            img = self.print_nodes(nodes, root_id, width + 1, depth + 1)
+            imgs.append(img)
 
-        return img
+        return imgs
 
 
-if __name__ == "__main__":
-    trees_example = [
-        {
-            "root": 0,
-            "nodes": {
-                0: {
-                    "children": [1, 2],  # List[int]
-                    "next_layer": [],  # List[int]
-                    "key_cnt": 2,
-                },
-                1: {
-                    "children": [],  # List[int]
-                    "next_layer": [],  # List[int]
-                    "key_cnt": 8,
-                },
-                2: {
-                    "children": [],  # List[int]
-                    "next_layer": [],  # List[int]
-                    "key_cnt": 16,
-                }
-            }
-        }
-    ]
-    with open("../acl1_100.json") as f:
-        trees = json.load(f)
-    gen = HeatMapGen(trees)
-    img = gen.generate_heatmap()
-    pprint(gen.trees)
-    print(img)
-    fig, ax = plt.subplots()
+def add_colorbar(fig, ax, last_img):
+    # https://stackoverflow.com/questions/13784201/matplotlib-2-subplots-1-colorbar
+    # # normalize colors to minimum and maximum values of dataset
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
+    cmap = 'plasma'
+    ncontours = 10
+    cmap = cm.get_cmap(cmap, ncontours)  # number of colors on colorbar
+    m = cm.ScalarMappable(cmap=cmap, norm=norm)
+    m.set_array([])
+    #im_ratio = last_img.shape[0] / last_img.shape[1]
+    cbar_top = fig.colorbar(m, #ax=ax,
+                            orientation='vertical',
+                            #fraction=5 * im_ratio,
+                            shrink=0.75, pad=0.2)  # , cax=cax_top)
+    cbar_top.set_ticks(np.linspace(0, 1, ncontours))
+
+
+def main(data):
+    gen = HeatMapGen(data)
+    imgs = gen.generate_heatmap()
+    # pprint(gen.trees)
+    # print(img)
+    plots = len(gen.trees)
+    # plots = 2
+    # x_max = max(len(img[0]) for img in imgs)
+    y_max = max(len(img) for img in imgs)
+
+    #  * -> y
+    #  |
+    #  v
+    #  X
+
+    fig, axs = plt.subplots(1, plots, sharey="row")
+    if plots == 1:
+        axs = (axs,)
 
     # We want to show all ticks...
-    ax.set_xticks(np.arange(len(img)))
-    ax.set_yticks(np.arange(len(img[0])))
+    # ax.set_xticks(np.arange(len(img)))
+    # ax.set_yticks(np.arange(len(img[0])))
     # ... and label them with the respective list entries
     # ax.set_xticklabels(farmers)
     # ax.set_yticklabels(vegetables)
@@ -132,8 +189,67 @@ if __name__ == "__main__":
     #        text = ax.text(j, i, harvest[i, j],
     #                       ha="center", va="center", color="w")
     #
-    im = ax.imshow(img, origin='lower')
+    # cbar = fig.colorbar(im, ax=ax, **cbar_kw)
+    # cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    for ax, img in zip(axs, imgs):
+        # ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.set_ylim((0, y_max))
+        # ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        # ax.xaxis.set_ticks([])
+        # ax.set_xscale("log")
+        im = ax.imshow(img,
+                       # cmap="YlGn",
+                        origin='lower', aspect="auto", vmin=0, vmax=1,
+                        )
 
-    ax.set_title("Memory usage efficiency")
+    add_colorbar(fig, axs[-1], imgs[-1])
+    # fig.set_title("Memory usage efficiency")
     fig.tight_layout()
+    plt.legend()
     plt.show()
+
+
+ROOT = os.path.join(os.path.dirname(__file__), "..")
+APP = os.path.join(ROOT, "build/meson.debug.linux.x86_64/benchmarks/pcv_mem_efficiency_report")
+
+
+def run_analysis(file):
+    j = check_output([APP, file])
+    return json.loads(j)
+
+
+if __name__ == "__main__":
+    #trees_example = [
+    #    {
+    #        "root": 0,
+    #        "nodes": {
+    #            0: {
+    #                "children": [1, 2],  # List[int]
+    #                "next_layer": [],  # List[int]
+    #                "key_cnt": 2,
+    #            },
+    #            1: {
+    #                "children": [],  # List[int]
+    #                "next_layer": [],  # List[int]
+    #                "key_cnt": 8,
+    #            },
+    #            2: {
+    #                "children": [],  # List[int]
+    #                "next_layer": [],  # List[int]
+    #                "key_cnt": 16,
+    #            }
+    #        }
+    #    }
+    #]
+    # fname = "../fw2_500.json"
+    fname = os.path.join(ROOT, "../classbench-ng/generated",
+                         #"acl1_100"
+                         #"acl2_100"
+                         "acl1_5000"
+                         # "fw2_5000"
+                         #"exact0_1024"
+                         #"exact0_8192"
+                         )
+    trees = run_analysis(fname)
+    # trees = trees_example
+    main(trees)

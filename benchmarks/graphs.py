b82923ec6a2343fcdd4e76efeda217998edeaa97
nic30 <nic30@seznam.cz>
2019-07-17 21:06:17 +0200

    prepare box plots for graphs

--- benchmarks/graphs.py
+++ benchmarks/graphs.py
@@ -1,7 +1,8 @@
 import sqlite3
 from os.path import basename
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
+import json
+
 
 def get_last_exec_time(db_conn):
     res = db_conn.execute('SELECT MAX(timestamp) FROM benchmark_execs')
@@ -10,19 +11,22 @@ def get_last_exec_time(db_conn):
 
 def generate_graph_throughput_with_increasing_number_of_flows(db_file_name):
     c = sqlite3.connect(db_file_name)
-    #last_exec = 0
+    # last_exec = 0
     last_exec = get_last_exec_time(c)
 
-    query = "SELECT * from test_result WHERE timestamp >= ? ORDER BY flow_cnt"
+    query = """
+    SELECT app_name, ruleset_name, flow_cnt, packet_cnt, lookup_speed, packet_lookup_times
+    FROM test_result WHERE timestamp >= ? ORDER BY flow_cnt
+    """
     flow_cnt_values = []  # x
     app_namesXruleset_throughput = {}  # name of series :  lists of troughtputs for different rulesetns (y)
     last_packet_cnt = None
-    for (_, app_name, rule_file, flow_cnt, packet_cnt, real_rule_cnt,
-            construction_time, number_of_tries_or_tables, lookup_speed) in c.execute(query, (last_exec,)):
+    for (app_name, ruleset_name, flow_cnt, packet_cnt, lookup_speed, packet_lookup_times
+            ) in c.execute(query, (last_exec,)):
         assert last_packet_cnt == None or packet_cnt == last_packet_cnt, (packet_cnt, last_packet_cnt)
         last_packet_cnt = packet_cnt
 
-        app_namesXruleset = "%s_%s" % (basename(app_name), basename(rule_file))
+        app_namesXruleset = "%s_%s" % (basename(app_name), basename(ruleset_name))
         throughput = app_namesXruleset_throughput.get(app_namesXruleset, [])
         app_namesXruleset_throughput[app_namesXruleset] = throughput
 
@@ -55,7 +59,6 @@ def generate_graph_throughput_with_increasing_number_of_flows(db_file_name):
     plt.savefig('fig/throughput_with_increasing_number_of_flows.png')
 
 
-
 def generate_ruleset_size_graph(order, data, x):
     """
     [ruleset size]  |
@@ -76,10 +79,11 @@ def generate_ruleset_size_graph(order, data, x):
     plt.grid()
     plt.savefig('fig/size_of_ruleset.png')
 
+
 def generate_graph_throughput_for_ruleset(ruleset_order, app_data, x, file_name):
     """
     [throughput]  |
-                 |-----
+                  |-----
                      [rulesets ordered by decreasing lookup speed]
     """
     fig, ax = plt.subplots(figsize=(20, 8))
@@ -87,11 +91,15 @@ def generate_graph_throughput_for_ruleset(ruleset_order, app_data, x, file_name)
     for name, data in app_data.items():
         y = [ d[4] for d in sorted(data, key=lambda d: ruleset_order[d[0]], reverse=True)]
         line1, = ax.plot(x, y, label=name)
+        packet_lookup_times = data[-1]
+        packet_lookup_times = json.loads(packet_lookup_times.decode("utf-8"))
+        ax.boxplot(packet_lookup_times)
+
 
     ax.set_ylabel('Throughput [MPkt/s]')
     ax.set_xlabel('rule set')
     ax.set_xlim(left=0)
-    #ax1.set_xticks(list(range(len(x))))
+    # ax1.set_xticks(list(range(len(x))))
     ax.set_xticklabels(x, minor=False, rotation=-90)
 
     # ax.legend()
@@ -105,6 +113,7 @@ def generate_graph_throughput_for_ruleset(ruleset_order, app_data, x, file_name)
 
     plt.grid()
     plt.savefig(file_name)
+
 
 def generate_graph_construction_time_for_ruleset(ruleset_order, app_data, x, file_name):
     """
@@ -125,7 +134,7 @@ def generate_graph_construction_time_for_ruleset(ruleset_order, app_data, x, fil
     ax.set_ylabel('Update time [us]')
     ax.set_xlabel('rule set')
     ax.set_xlim(left=0)
-    #ax1.set_xticks(list(range(len(x))))
+    # ax1.set_xticks(list(range(len(x))))
     ax.set_xticklabels(x, minor=False, rotation=-90)
 
     # ax.legend()
@@ -139,6 +148,7 @@ def generate_graph_construction_time_for_ruleset(ruleset_order, app_data, x, fil
 
     plt.grid()
     plt.savefig(file_name)
+
 
 def rulesets_ordered_by_cummulative_key(app_data, val_index, reverse):
     cumul_for_ruleset = {}
@@ -155,6 +165,7 @@ def rulesets_ordered_by_cummulative_key(app_data, val_index, reverse):
     x = [_x[0] for _x in x]
     return cumul_for_ruleset, x
 
+
 def generate_tree_cnt_for_ruleset(ruleset_order, app_data, x, file_name):
     """
     [tree cnt]  |
@@ -168,11 +179,11 @@ def generate_tree_cnt_for_ruleset(ruleset_order, app_data, x, file_name):
         # print(y)
         line1, = ax.plot(x, y, label=name)
 
-    #ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    # ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_ylabel('trie/tree/table cnt')
     ax.set_xlabel('rule set')
     ax.set_xlim(left=0)
-    #ax1.set_xticks(list(range(len(x))))
+    # ax1.set_xticks(list(range(len(x))))
     ax.set_xticklabels(x, minor=False, rotation=-90)
 
     # ax.legend()
@@ -187,6 +198,7 @@ def generate_tree_cnt_for_ruleset(ruleset_order, app_data, x, file_name):
     plt.grid()
     plt.savefig(file_name)
 
+
 def generate_graphs_depending_on_ruleset_size(db_file_name, number_of_flows, pkt_cnt):
     """
     [y]  |
@@ -195,28 +207,34 @@ def generate_graphs_depending_on_ruleset_size(db_file_name, number_of_flows, pkt
     """
 
     c = sqlite3.connect(db_file_name)
-    #last_exec = 0
+    # last_exec = 0
     last_exec = get_last_exec_time(c)
 
-    query = """SELECT app_name, ruleset_name, real_rule_cnt,
-                       construction_time, number_of_tries_or_tables, lookup_speed
-               from test_result WHERE
-               packet_cnt == ? AND flow_cnt == ? AND timestamp >= ? ORDER BY flow_cnt"""
+    query = """
+    SELECT app_name, ruleset_name, real_rule_cnt,
+        construction_time, number_of_tries_or_tables, lookup_speed, packet_lookup_times
+    FROM test_result WHERE
+    packet_cnt == ? AND flow_cnt == ? AND timestamp >= ? ORDER BY flow_cnt
+    """
     # group by app name
-    app_data = {} # {name: (name of rulse_set, construction_time, number_of_tries_or_tables, lookup_speed)}
-    for (app_name, rule_file, real_rule_cnt, construction_time, number_of_tries_or_tables, lookup_speed
-            ) in c.execute(query, (pkt_cnt, number_of_flows, last_exec,)):
+    app_data = {}  # {name: (name of rulse_set, construction_time, number_of_tries_or_tables, lookup_speed)}
+    for (app_name, rule_file, real_rule_cnt, construction_time,
+         number_of_tries_or_tables, lookup_speed, packet_lookup_times
+         ) in c.execute(query, (pkt_cnt, number_of_flows, last_exec,)):
 
         app_names = basename(app_name)
         rule_file = basename(rule_file)
         data = app_data.get(app_names, [])
         app_data[app_names] = data
-        data.append((rule_file, real_rule_cnt, construction_time, number_of_tries_or_tables, lookup_speed))
+        data.append((
+            rule_file, real_rule_cnt, construction_time,
+            number_of_tries_or_tables, lookup_speed,
+            packet_lookup_times))
 
     # it is expected that there is single data record for each rule set
 
     # used to sort the rulesets
-    #(rule_file, _, _, _, lookup_speed)
+    # (rule_file, _, _, _, lookup_speed)
     cumul_throughput_for_ruleset, x = rulesets_ordered_by_cummulative_key(app_data, 4, True)
 
     data2_values = list(app_data.values())[0]

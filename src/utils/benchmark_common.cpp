73bfd5bdee5ab194abca7ff4cdaa6d60fc7b8909
Nic30 <nic30@seznam.cz>
2019-11-01 16:17:38 +0100

    BenchmarkStats: support for test which are not measuring packet cls. t.

--- src/utils/benchmark_common.cpp
+++ src/utils/benchmark_common.cpp
@@ -4,7 +4,7 @@
 namespace pcv {
 
 BenchmarkStats::BenchmarkStats(size_t LOOKUP_CNT, size_t real_rule_cnt,
-		size_t trace_cnt, std::ostream & out) :
+		size_t trace_cnt, std::ostream &out) :
 		LOOKUP_CNT(LOOKUP_CNT), out(out), construction_timer(nullptr), lookup_timer(
 				nullptr), real_rule_cnt(real_rule_cnt), number_of_tries_or_tables(
 				-1), ns_per_packet(trace_cnt) {
@@ -25,7 +25,8 @@ void BenchmarkStats::lookup_packet_start() {
 
 void BenchmarkStats::lookup_packet_stop(size_t p_id) {
 	auto finish = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<uint64_t, std::nano> t = finish - actual_packet_start;
+	std::chrono::duration < uint64_t, std::nano > t = finish
+			- actual_packet_start;
 	ns_per_packet[p_id] += t.count();
 }
 
@@ -41,22 +42,30 @@ void BenchmarkStats::set_number_of_tries_or_tables(
 	this->number_of_tries_or_tables = number_of_tries_or_tables;
 }
 
-void BenchmarkStats::dump(std::function<void(std::ostream &)> json_extra,
-		std::function<void(std::ostream &)> text_extra) {
+void BenchmarkStats::dump(std::function<void(std::ostream&)> json_extra,
+		std::function<void(std::ostream&)> text_extra) {
 	uint64_t lookup_time_from_packets = 0;
 	for (auto t : ns_per_packet)
 		lookup_time_from_packets += t;
-
-	auto lookup_speed = ((LOOKUP_CNT / double(lookup_time_from_packets))
-			* 1000.0);
+	double lookup_speed;
+	if (lookup_time_from_packets == 0) {
+		// the packet time was not used at all
+		lookup_speed = (double(LOOKUP_CNT)/ lookup_timer->us());
+	} else {
+		lookup_speed =
+				((LOOKUP_CNT / double(lookup_time_from_packets)) * 1000.0);
+	}
 	//out << lookup_time_from_packets << " " << lookup_timer->us() * 1000 << std::endl;
 	//auto lookup_speed = (LOOKUP_CNT / double(lookup_timer->us()));
 	if (std::isinf(lookup_speed))
 		lookup_speed = 0.0; // time can be lower than 1us and json can not contain inf
 	out << "{ \"lookup_speed\":" << lookup_speed << "," << std::endl;  // Mpkt/s
-	out << "\"minimal_benchmark_overhead\":"
-			<< (lookup_timer->us() * 1000 / double(lookup_time_from_packets))
-			<< "," << std::endl; // (1 = zero overhead caused by precise timers)
+	auto overhead = (lookup_timer->us() * 1000
+			/ double(lookup_time_from_packets));
+	if (std::isinf(overhead))
+		overhead = 0.0;
+
+	out << "\"minimal_benchmark_overhead\":" << overhead << "," << std::endl; // (1 = zero overhead caused by precise timers)
 	out << "\"construction_time\":" << uint64_t(construction_timer->us()) << "," // us
 			<< std::endl;
 	out << "\"real_rule_cnt\":" << real_rule_cnt << "," << std::endl;
